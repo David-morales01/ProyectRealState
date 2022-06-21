83a1a10bc9bcd16e16a0bf14c9da333466691516
Davis-morales01 <davidenriquemorales4@gmail.com>
2022-06-21 14:15:45 -0600

    add file

---
+++ RealStateApp/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Routes, Route,useRoutes} from 'react-router-dom' 
+import { ChakraProvider} from '@chakra-ui/react' 
+import Home from './components/Home/Home' 
+import Login from './components/Auth/Login/Login'
+import Register from './components/Auth/Register/Register'    
+ import Theme from './Theme/Theme.js'
+
+ export default function App() {
+  return ( 
+  <ChakraProvider theme ={Theme}>
+    <Routes>
+      <Route element ={<Home/>} path="/" />
+      <Route element ={<Login/>} path="login" />
+      <Route element ={<Register/>} path="register" /> 
+    </Routes>
+    
+  </ChakraProvider>  
+  );
+}
+
---
+++ RealStateApp/src/main.jsx
@@ -0,0 +1,31 @@
+/* import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+*/
+
+
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App'; 
+import {BrowserRouter} from 'react-router-dom';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <StrictMode>
+    <ColorModeScript />
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </StrictMode>
+);
+
+ 
