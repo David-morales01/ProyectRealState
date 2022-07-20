d888cbf3e33af1a017c03c638b5204e4e05e4790
Davis-morales01 <davidenriquemorales4@gmail.com>
2022-07-20 12:31:16 -0600

    add file

--- RealStateApp/src/components/UI/MapBox/MapBox.jsx
+++ RealStateApp/src/components/UI/MapBox/MapBox.jsx
@@ -1,17 +1,17 @@
-import React,{useEffect,useRef} from 'react' 
+import React,{useEffect,useRef} from 'react'
 import {Box} from '@chakra-ui/react'
 import mapboxgl,{Map} from 'mapbox-gl'
 import MapStore from '../../../Store/MapStore'
 import AuthStore from '../../../Store/AuthStore'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import Spinner from '../Spinner/Spinner' 
+import Spinner from '../Spinner/Spinner'
 import FormMarker from '../Modal/FormMarker'
 import ErrorMarker from '../Message/ErrorMarker'
 import './marker.scss'
 import * as ReactDOM from 'react-dom/client'
 import Popup from './Popup'
 
-export default function MapBox(){ 
+export default function MapBox(){
 
     // Store
     const mapStore = MapStore()
@@ -29,12 +29,12 @@ export default function MapBox(){
     const mapDiv =useRef(null)
     const map =useRef(null)
     // Token
-    mapboxgl.accessToken = `${import.meta.env.VITE_REACT_APP_MAPBOX_TOKEN}` 
+    mapboxgl.accessToken = `${import.meta.env.VITE_REACT_APP_MAPBOX_TOKEN}`
 
-    useEffect(()=>{  
+    useEffect(()=>{
         if(markers.length<1){
             console.log('getMarkers')
-            mapStore.getMarkers() 
+            mapStore.getMarkers()
         }
         map.current = new Map({
             container: mapDiv.current,
@@ -44,76 +44,82 @@ export default function MapBox(){
         })
         const nav = new mapboxgl.NavigationControl({
             visualizePitch: false
-        }); 
-                    
-        map.current.addControl(nav, 'top-right');  
+        });
+
+        map.current.addControl(nav, 'top-right');
 
         if(user.rol == 'admin'){
-            map.current.on('click',function(e){  
-                mapStore.getCoordinate(e.lngLat) 
-            }) 
-        } 
+            map.current.on('click',function(e){
+                mapStore.getCoordinate(e.lngLat)
+            })
+        }
         changeListMarkers()
-    },[]) 
+    },[])
 
-    useEffect(()=>{ 
+    useEffect(()=>{
         const removeMarker = document.querySelectorAll('.mapboxgl-marker')
         if(removeMarker){
             removeMarker.forEach(function(marker) {
                 marker.remove();
             });
-        } 
-            
-        if(markers.length >1 && listMarkers ==true){ 
-            markers.map((marker)=>{  
-                let imgMarker =''
-                const lengthImage = marker.images.length
-                if(lengthImage>0){
-                    marker.images.forEach((img)=>{
-                        imgMarker = imgMarker + `<img src='${import.meta.env.VITE_REACT_APP_ROUTE_STORAGE}/${img.src_img}'/ alt='Image Marker'>`
-                    })
+        }
 
-                }else{
-                    imgMarker = '<div class="defaultImage">No image available</div>'
-                }
+        if(markers.length >1 && listMarkers ==true){
+            markers.map((marker)=>{
+                // let imgMarker =''
+                // const lengthImage = marker.images.length
+                // if(lengthImage>0){
+                //     marker.images.forEach((img)=>{
+                //         imgMarker = imgMarker + `<img src='${import.meta.env.VITE_REACT_APP_ROUTE_STORAGE}/${img.src_img}'/ alt='Image Marker'>`
+                //     })
+
+                // }else{
+                //     imgMarker = '<div class="defaultImage">No image available</div>'
+                // }
                 let markerColor = '#4671FF';
                 if(user.id == marker.user_id){
                     markerColor ='#FF3333'
                 }
-                const popup =    new mapboxgl.Popup({ offset: 25 }) 
+                const popup =    new mapboxgl.Popup({ offset: 25 })
                 .setHTML(
-                    `<div  class='divMarker' ><div>`
+
+                    `<div  class='divMarker' id='divMarker' ><div>`
                 )
                 popup.on('open', () => {
-                    const container = document.querySelector('.divMarker')
-                    const root = ReactDOM.createRoot(container) 
+
+                    const container = document.querySelector('.mapboxgl-popup-content')
+                    const root = ReactDOM.createRoot(container)
                     root.render(
-                    <> 
-                        <Popup/>
-                    </>
-                )
+                        <>
+                            <Popup marker={marker}/>
+                        </>
+                    )
                     console.log('popup was opened');
                 })
-                popup.on('close', () => {
-                    // const container = document.querySelector('.divMarker')
-                    // container.remove()
-                    console.log('popup was closed');
-                    });
+                // popup.on('close', () => {
+                //     const container = document.querySelector('.mapboxgl-popup-content')
+                //     if(container){
+
+                //     container.remove()
+                //     console.log('popup was closed');
+                //     }
+                //     console.log('popup was 1 closed');
+                //     });
                 const arrCoordinate = [marker.long,marker.lat]
                 new mapboxgl.Marker({ color: markerColor,fontSize:'90px' }).setLngLat(arrCoordinate)
                 .setPopup(
                     popup
-                ).addTo(map.current); 
+                ).addTo(map.current);
             })
-                
-        } 
+
+        }
     },[markers])
 
     return (
         <>
             <Box w='100%' h='100%'  ref={mapDiv} position='relative'></Box>
             {statusMap? '':<Spinner backGround='rgba(0, 0, 0, 0.9)' />}
-            {coordinate? <FormMarker /> :''} 
+            {coordinate? <FormMarker /> :''}
             {error ? <ErrorMarker/>:''}
         </>
     )
--- RealStateApp/src/components/UI/MapBox/Popup.jsx
+++ RealStateApp/src/components/UI/MapBox/Popup.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 
-export default function Popup(){
+export default function Popup({marker}){
+    console.log(marker)
     return(
         <> soy popup</>
     )
